0cd0eb110ad4841b7b4a92536f9045183959ebf9
Boon Hui <boonhui@blockfi.com>
2020-10-09 10:37:00 +0800

    fix controller test

--- test/todolist_web/controllers/page_controller_test.exs
+++ test/todolist_web/controllers/page_controller_test.exs
@@ -1,8 +1,15 @@
 defmodule TodolistWeb.PageControllerTest do
   use TodolistWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
+  setup %{conn: conn} do
+    user = %Todolist.Users.User{email: "test@example.com", id: 1}
+    authed_conn = Pow.Plug.assign_current_user(conn, user, [])
+
+    {:ok, conn: conn, authed_conn: authed_conn}
+  end
+
+  test "GET /", %{authed_conn: authed_conn} do
+    conn = get(authed_conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
