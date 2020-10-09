cb33806036d0790404ba6e7794d63b7617aee6d3
Boon Hui <boonhui@blockfi.com>
2020-10-09 15:14:03 +0800

    simplify test setup

--- test/todolist_web/controllers/page_controller_test.exs
+++ test/todolist_web/controllers/page_controller_test.exs
@@ -2,16 +2,11 @@ defmodule TodolistWeb.PageControllerTest do
   use TodolistWeb.ConnCase
 
   setup %{conn: conn} = config do
-    if config[:logged_in] == true do
-      user = %Todolist.Users.User{email: "test@example.com", id: 1}
-      authed_conn = Pow.Plug.assign_current_user(conn, user, [])
-      {:ok, conn: conn, authed_conn: authed_conn}
-    else
-      {:ok, conn: conn}
-    end
+    user = %Todolist.Users.User{email: "test@example.com", id: 1}
+    authed_conn = Pow.Plug.assign_current_user(conn, user, [])
+    {:ok, conn: conn, authed_conn: authed_conn}
   end
 
-  @tag logged_in: true
   test "GET / when authenticated", %{authed_conn: authed_conn} do
     conn = get(authed_conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
