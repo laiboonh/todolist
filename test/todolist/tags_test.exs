b440627ee5d0f039286f86bc8a27aa49258e021d
Boon Hui <boonhui@blockfi.com>
2020-10-14 18:29:24 +0800

    update test to make use of setup macro

--- test/todolist/tags_test.exs
+++ test/todolist/tags_test.exs
@@ -3,61 +3,64 @@ defmodule Todolist.TagsTest do
 
   alias Todolist.Tags
 
+  setup do
+    %{
+      valid_attrs: %{name: "some name"},
+      update_attrs: %{name: "some updated name"},
+      invalid_attrs: %{name: nil}
+    }
+  end
+
   describe "tags" do
     alias Todolist.Tags.Tag
-
-    @valid_attrs %{name: "some name"}
-    @update_attrs %{name: "some updated name"}
-    @invalid_attrs %{name: nil}
 
     def tag_fixture(attrs \\ %{}) do
       {:ok, tag} =
         attrs
-        |> Enum.into(@valid_attrs)
         |> Tags.create_tag()
 
       %Tag{tag | tasks: []}
     end
 
-    test "list_tags/0 returns all tags" do
-      tag = tag_fixture()
+    test "list_tags/0 returns all tags", context do
+      tag = tag_fixture(context.valid_attrs)
       assert Tags.list_tags() == [tag]
     end
 
-    test "get_tag!/1 returns the tag with given id" do
-      tag = tag_fixture()
+    test "get_tag!/1 returns the tag with given id", context do
+      tag = tag_fixture(context.valid_attrs)
       assert Tags.get_tag!(tag.id) == tag
     end
 
-    test "create_tag/1 with valid data creates a tag" do
-      assert {:ok, %Tag{} = tag} = Tags.create_tag(@valid_attrs)
+    test "create_tag/1 with valid data creates a tag", context do
+      assert {:ok, %Tag{} = tag} = Tags.create_tag(context.valid_attrs)
       assert tag.name == "some name"
     end
 
-    test "create_tag/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Tags.create_tag(@invalid_attrs)
+    test "create_tag/1 with invalid data returns error changeset", context do
+      assert {:error, %Ecto.Changeset{}} = Tags.create_tag(context.invalid_attrs)
     end
 
-    test "update_tag/2 with valid data updates the tag" do
-      tag = tag_fixture()
-      assert {:ok, %Tag{} = tag} = Tags.update_tag(tag, @update_attrs)
+    test "update_tag/2 with valid data updates the tag", context do
+      tag = tag_fixture(context.valid_attrs)
+      assert {:ok, %Tag{} = tag} = Tags.update_tag(tag, context.update_attrs)
       assert tag.name == "some updated name"
     end
 
-    test "update_tag/2 with invalid data returns error changeset" do
-      tag = tag_fixture()
-      assert {:error, %Ecto.Changeset{}} = Tags.update_tag(tag, @invalid_attrs)
+    test "update_tag/2 with invalid data returns error changeset", context do
+      tag = tag_fixture(context.valid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Tags.update_tag(tag, context.invalid_attrs)
       assert tag == Tags.get_tag!(tag.id)
     end
 
-    test "delete_tag/1 deletes the tag" do
-      tag = tag_fixture()
+    test "delete_tag/1 deletes the tag", context do
+      tag = tag_fixture(context.valid_attrs)
       assert {:ok, %Tag{}} = Tags.delete_tag(tag)
       assert_raise Ecto.NoResultsError, fn -> Tags.get_tag!(tag.id) end
     end
 
-    test "change_tag/1 returns a tag changeset" do
-      tag = tag_fixture()
+    test "change_tag/1 returns a tag changeset", context do
+      tag = tag_fixture(context.valid_attrs)
       assert %Ecto.Changeset{} = Tags.change_tag(tag)
     end
 
